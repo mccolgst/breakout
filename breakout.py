6c19157bcca3e7799e3b6039b9526d0097f250ff
mccolgst <mccolgst@gmail.com>
2010-11-05 19:29:21 -0400

    added transitions between level, and a 'you win' screen

--- breakout.py
+++ breakout.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 DIRECTIONS = {'none': -1, 'left':0, 'right': 1, 'down' : 2, 'up':3, 'downleft':4, 'downright':5, 'upleft':6, 'upright':7}
 MOVESPEED = 4
-BALLSPEED = 3
+BALLSPEED = 2
 
 pygame.init()
 screen = pygame.display.set_mode((460, 460), 0, 32)
@@ -14,7 +14,7 @@ background = background.convert()
 background.fill((250,250,250))
 
 font = pygame.font.Font(None, 36)
-text = font.render("Pong", 1, (10, 10, 10))
+text = font.render("Breakout!", 1, (10, 10, 10))
 textpos = text.get_rect(centerx=background.get_width()/2)
 background.blit(text, textpos)
 
@@ -56,18 +56,20 @@ class Ball(object):
 		self.dy = BALLSPEED
 		self.direction = DIRECTIONS['downright']
 		self.rect = pygame.rect.Rect(self.x, self.y, self.width, self.height)
+		self.go = False
 
 	def update(self):
-		if self.rect.x >= background.get_width() - self.width:
-			self.change_direction('x')
-		elif self.rect.x <= 0:
-			self.change_direction('x')
-#		if self.rect.y >= background.get_height() - self.height:
-#			self.change_direction('y')
-		elif self.rect.y <= 0:
-			self.change_direction('y')
-		self.rect.x += self.dx
-		self.rect.y += self.dy
+		if self.go == True:
+			if self.rect.x >= background.get_width() - self.width:
+				self.change_direction('x')
+			elif self.rect.x <= 0:
+				self.change_direction('x')
+		#	if self.rect.y >= background.get_height() - self.height:
+		#		self.change_direction('y')
+			elif self.rect.y <= 0:
+				self.change_direction('y')
+			self.rect.x += self.dx
+			self.rect.y += self.dy
 		pygame.draw.rect(screen, (255,0,0), self.rect)
 
 	def change_direction(self, coord):
@@ -76,74 +78,96 @@ class Ball(object):
 		elif coord == 'y':
 			self.dy = self.dy * -1
 
+	def reset(self):
+		self.rect.x = background.get_width()/2 - 50
+		self.rect.y = 250
+
 class Game():
-    def __init__(self):
-        self.player = Player()
-        self.keyboard = Keyboard(self.player)
-        self.current_level = 0
-        self.blocks = self.make_blocks(self.load_level(self.current_level))
-        self.ball = Ball()
-        self.updateables = [self.player, self.ball]
-        self.levels = self.all_levels()
+	def __init__(self):
+		self.player = Player()
+		self.current_level = 0
+		self.ball = Ball()
+		self.blocks = []
+		self.keyboard = Keyboard(self.player, self.ball)
+		self.updateables = [self.player, self.ball]
+		self.levels = self.all_levels()
+		self.next_level()
 
-    def all_levels(self):
-        levels = [f for f in os.listdir('.') if f[-3:] == 'lvl']
-        return levels
+	def all_levels(self):
+		levels = [f for f in os.listdir('.') if f[-3:] == 'lvl']
+		return levels
 
-    def load_level(self, level = 0):
-        f = open('level'+str(level)+'.lvl')
-        leveldata = [l.rstrip().split(',') for l in f.readlines()]
-        return leveldata
+	def load_level(self, level = 0):
+		f = open('level'+str(level)+'.lvl')
+		leveldata = [l.rstrip().split(',') for l in f.readlines()]
+		return self.make_blocks(leveldata)
 
-    def next_level(self):
-        del self.blocks[:]
-		#todo: mid level transition text perhaps? (need to learn about scene transitions)
-        self.blocks = self.make_blocks(self.load_level(self.current_level))
+	def next_level(self):
+		del self.blocks[:]
+		self.blocks = self.load_level(self.current_level)
+		self.ball.reset()
+		self.ball.go = False
+		self.transition_screen("Level: %s" %str(self.current_level + 1))
+
+	def transition_screen(self, text):
+		transition = pygame.Surface(screen.get_size())
+		transition.fill((250,250,250))
+		font = pygame.font.Font(None, 36)
+		text = font.render(text, 1, (10, 10, 10))
+		textpos = text.get_rect(centerx=background.get_width()/2, centery=background.get_height()/2)
+		transition.blit(text, textpos)
+		screen.blit(transition, (0,0))
+		pygame.display.update()
+		pygame.time.wait(1500)
+		del transition
+
+	def win(self):
+		self.transition_screen("You win!")
 		
 
-    def make_blocks(self, leveldata): 
-        blocks = []
-        y = 40
-        for line in leveldata:
-            x = 50
-            for b in line:
-                if int(b) == 1:
-                    blocks.append(Block(x, y))
-                x += 70
-            y += 25
-        return blocks
+	def make_blocks(self, leveldata): 
+		blocks = []
+		y = 40
+		for line in leveldata:
+			x = 50
+			for b in line:
+				if int(b) == 1:
+					blocks.append(Block(x, y))
+				x += 70
+			y += 25
+		return blocks
 
-    def update(self):
-        keepGoing = True
-        keepGoing = self.keyboard.handle_input()
-        for u in self.updateables:
-            u.update()
+	def update(self):
+		keepGoing = True
+		keepGoing = self.keyboard.handle_input()
+		for u in self.updateables:
+			u.update()
 
-        for b in self.blocks:
-            b.update()
-            if self.ball.rect.colliderect(b.rect):
-                self.ball.change_direction('y')
-                self.blocks.pop(self.blocks.index(b))
+		for b in self.blocks:
+			b.update()
+			if self.ball.rect.colliderect(b.rect):
+				self.ball.change_direction('y')
+				self.blocks.pop(self.blocks.index(b))
 
-        if self.player.rect.colliderect(self.ball.rect):
-            self.ball.change_direction('y')
+		if self.player.rect.colliderect(self.ball.rect):
+			self.ball.change_direction('y')
 
-        if len(self.blocks) == 0: #won (this level)
-            self.current_level += 1
-            print self.current_level
-            print self.levels
-            if self.current_level >= len(self.levels):
-                keepGoing = False
-            else:
-                self.next_level()
-        if self.ball.rect.y >= background.get_height() - self.ball.height: #lost
-            keepGoing = False
-        return keepGoing
+		if len(self.blocks) == 0: #won (this level)
+			self.current_level += 1
+			if self.current_level >= len(self.levels):#won (the game)
+				self.win()
+				keepGoing = False
+			else:
+				self.next_level()
+		if self.ball.rect.y >= background.get_height() - self.ball.height: #lost (the game)
+			keepGoing = False
+		return keepGoing
 
 
 class Keyboard(object):
-	def __init__(self, player):#todo: fix, not sure if keyboardcontroller should require a player instance...
+	def __init__(self, player, ball):#todo: fix, not sure if keyboardcontroller should require a player instance...
 		self.player = player
+		self.ball = ball
 	
 	def handle_input(self):
 		for event in pygame.event.get():
@@ -158,19 +182,22 @@ class Keyboard(object):
 				if event.key == K_LEFT:
 					self.player.move = DIRECTIONS['left']
 
+				if event.key == K_SPACE and self.ball.go == False:
+					self.ball.go = True
+
 			if event.type == KEYUP:
 				self.player.move = 2 #todo: fix
 		return True
 
 def main():
-    c = pygame.time.Clock()
-    g = Game()
-    keepGoing = True
-    while keepGoing:
-        c.tick(100)
-        screen.blit(background, (0, 0))
-        keepGoing = g.update()
-        pygame.display.flip()
+	c = pygame.time.Clock()
+	game = Game()
+	keepGoing = True
+	while keepGoing:
+		c.tick(100)
+		screen.blit(background, (0, 0))
+		keepGoing = game.update()
+		pygame.display.flip()
 
 if __name__ == '__main__':
     main()
